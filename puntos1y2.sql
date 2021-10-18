8ea49aed333bfe88774305a976fc2d8a398fc490
Ailen Amodio <46222455+ailenamodio@users.noreply.github.com>
2021-10-18 11:57:52 -0300

    Add files via upload
    Resolución de los puntos 1 y 2

---
+++ puntos1y2.sql
@@ -0,0 +1,6 @@
+/*punto 1*/
+select count (ESTUDIANTE_legajo) where nombre='mecanica';
+/*punto 2*/
+select * from profesor where fecha_nacimiento between '1980-01-01' and
+'1989-12-31' order by salario asc;
+
